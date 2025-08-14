f6ffef443aef868133292019ecbb8abd9916405a
addya <aj11routa@yorksj.ac.uk>
2025-08-14 23:07:32 +0100

    move navbar javascript from Page2.js into NavbarComp.js

--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -1,9 +1,53 @@
+import React, { useState } from "react";
 import "../App.css";
 
 function NavbarComp() {
+  const [navbarFixed, setNavbarFixed] = useState(false);
+
+  let oldLastKnownScrollY = 0;
+  let newLastKnownScrollY = 0;
+  let ticking = false;
+
+  //check for scroll
+  document.addEventListener("scroll", (event) => {
+    console.log(window.scrollY);
+    newLastKnownScrollY = window.scrollY;
+
+    if (!ticking && newLastKnownScrollY < oldLastKnownScrollY) {
+      //only run this code if scrolling up
+      window.requestAnimationFrame(() => {
+        onScrollUp();
+        ticking = false;
+      });
+
+      ticking = true;
+    } else {
+      onScrollDown();
+    }
+
+    oldLastKnownScrollY = newLastKnownScrollY;
+  });
+
+  <script src="script.js" />;
+
+  function onScrollUp() {
+    console.log("going up");
+    setNavbarFixed(true);
+  }
+
+  function onScrollDown() {
+    console.log("going down");
+    setNavbarFixed(false);
+  }
+
   return (
     <div>
       <div className={"navbar"}>NAVBAR!</div>
+      {navbarFixed === true && (
+        <div className={navbarFixed ? "navbar-sticky" : ""} id="navbar">
+          NAVBAR!
+        </div>
+      )}
     </div>
   );
 }
--- src/Pages/Page2.js
+++ src/Pages/Page2.js
@@ -11,61 +11,15 @@ import { Link } from "react-router-dom";
 3. on scroll down, have nav be scrolled out of view (instead of immediately disappearing) 
 
 */
+
 function Page2() {
-  const [navbarFixed, setNavbarFixed] = useState(false);
-
-  let oldLastKnownScrollY = 0;
-  let newLastKnownScrollY = 0;
-  let ticking = false;
-
-  //check for scroll
-  document.addEventListener("scroll", (event) => {
-    console.log(window.scrollY);
-    newLastKnownScrollY = window.scrollY;
-
-    if (!ticking && newLastKnownScrollY < oldLastKnownScrollY) {
-      //only run this code if scrolling up
-      window.requestAnimationFrame(() => {
-        onScrollUp();
-        ticking = false;
-      });
-
-      ticking = true;
-    } else {
-      onScrollDown();
-    }
-
-    oldLastKnownScrollY = newLastKnownScrollY;
-  });
-
-  function onScrollUp() {
-    console.log("going up");
-    setNavbarFixed(true);
-  }
-
-  function onScrollDown() {
-    console.log("going down");
-    setNavbarFixed(false);
-  }
-
-  function clickHappen() {
-    console.log("click");
-    setNavbarFixed(!navbarFixed);
-  }
-
   return (
     <div>
       <div className="header">
         <NavbarComp />
-        {navbarFixed === true && (
-          <div className={navbarFixed ? "navbar-still" : ""} id="navbar">
-            NAVBAR!
-          </div>
-        )}
         PAGE 2
       </div>
       <div className="content">
-        <button onClick={clickHappen}>Toggle sticky</button>
         <Link to="/">
           <button className="my-button">Go to page 1</button>
         </Link>
@@ -102,7 +56,6 @@ function Page2() {
           hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
           per conubia nostra inceptos himenaeos.
         </p>
-        <button onClick={clickHappen}>Toggle sticky</button>
 
         <p>
           Lorem ipsum 22222222222 dolor sit amet consectetur adipiscing elit.
